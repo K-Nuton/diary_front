49b9719f744b3687157daa383668bc80a09e979e
Tjoakta04130 <8thonayah0@gmail.com>
2020-10-20 23:22:49 +0900

    date pickerのためのプロバイダーの提供箇所を Appコンポーネントの中に格納

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
 const useStyles = makeStyles({
   button: {
@@ -14,9 +16,11 @@ const useStyles = makeStyles({
 function App() {
   const classes = useStyles();
   return (
-    <Button className={`${classes.button} ${classes.red}`} variant="contained" color="primary">
-      Hello World
-    </Button>
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Button className={`${classes.button} ${classes.red}`} variant="contained" color="primary">
+        Hello World
+      </Button>
+    </MuiPickersUtilsProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
 
 ReactDOM.render(
   <React.StrictMode>
-  <MuiPickersUtilsProvider utils={DateFnsUtils}>
     <App />
-  </MuiPickersUtilsProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
