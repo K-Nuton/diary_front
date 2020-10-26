71677542abc1476a200571628c36f7f051e1c25b
Tjoakta04130 <8thonayah0@gmail.com>
2020-10-27 07:27:04 +0900

    コピーライトを Takanori Kusumoto に変更

--- src/Login.tsx
+++ src/Login.tsx
@@ -15,7 +15,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        Takanori Kusumoto
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
