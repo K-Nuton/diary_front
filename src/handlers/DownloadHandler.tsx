02e8f21457fecc5740e50f2ed5cf60858892e1bf
Tjoakta04130 <8thonayah0@gmail.com>
2020-10-29 22:54:57 +0900

    日付文字列をAPIに対応する形式に修正

--- src/handlers/DownloadHandler.tsx
+++ src/handlers/DownloadHandler.tsx
@@ -4,15 +4,24 @@ export function downloadDiaries(json: SearchResponse): void {
   const result = window.confirm('日記JSONをダウンロードします');
   if (!result) return;
 
+  addZero(json);
   const fileStr = JSON.stringify(json);
 
   const blob = new Blob(
     [fileStr],
-    {"type": "text/plain"}
+    { "type": "text/plain" }
   );
 
   const link = document.createElement('a');
   link.href = window.URL.createObjectURL(blob);
   link.download = 'diaries.json';
   link.click();
+}
+
+function addZero(json: SearchResponse): void {
+  const ADD = ".0";
+  for (let i = 0, len = json.diaries.length; i < len; i++) {
+    json.diaries[i].date += ADD;
+    json.diaries[i].update_date += ADD;
+  }
 }
